8d312504a52c95962ec17670d9222d0ec05bd24a
DashaDasha88 <dasha.kotova.mail@gmail.com>
2021-03-30 18:21:43 +0000

    add business logic to classes and exercises 5-7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Dasha", last_name: "Kotova", hourly_rate: 100)
+@store2.employees.create(first_name: "Bob", last_name: "Johnson", hourly_rate: 30)
+@store1.employees.create(first_name: "Joe", last_name: "Mama", hourly_rate: 40)
+@store1.employees.create(first_name: "Trixie", last_name: "Mattel", hourly_rate: 100)
+@store2.employees.create(first_name: "Kittie", last_name: "Gurl", hourly_rate: 120)
